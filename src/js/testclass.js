a369e81908cb53bdf42e7d41e593d3511967f7e0
David Lochhead <dave@defmech.com>
2017-01-25 16:37:13 +0000

    Added a setTimeout for a quick test.

--- src/js/testclass.js
+++ src/js/testclass.js
@@ -3,6 +3,10 @@ const name = 'TestClassName';
 export default class TestClass {
   constructor() {
     console.log('testclass.js', 'TestClass constructor');
+
+    setTimeout(() => {
+      console.log('testclass.js', 'After delay');
+    }, 2000);
   }
 
   toString() {
